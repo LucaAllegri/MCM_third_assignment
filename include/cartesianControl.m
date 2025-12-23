f106623df711393b91aa8da7d5521e7eddd3ab15
Luca Allegri <luca.allegri02@gmail.com>
2025-12-23 10:49:56 +0100

    last point end added rigid body

--- include/cartesianControl.m
+++ include/cartesianControl.m
@@ -32,23 +32,40 @@ classdef cartesianControl < handle
             T_b_t = self.gm.getToolTransformWrtBase();
             O_b_t =  T_b_t(1:3,4);
             O_b_g = bTg(1:3,4);
-            r = O_b_g - O_b_t; % Compute position error
+
+            tTg = inv(T_b_t) * bTg;
+
+            [h, theta] = RotToAngleAxis(tTg(1:3,1:3));
+
+            rho_tg = h*theta;
+            r_tg = tTg(1:3,4);
+
+            b_rho_tg = T_b_t(1:3,1:3) * rho_tg;
+            b_r_tg = T_b_t(1:3,1:3) * r_tg;
+
+            b_e_tg = [b_rho_tg; b_r_tg];
             
-            %% errore sull'angolo test
-            bRt = T_b_t(1:3,1:3);
-            bRg = bTg(1:3,1:3);
-            Rerr = bRg*bRt'; % cambia l'ordine
-            [h, theta] = RotToAngleAxis(Rerr);
-            e = [h*theta; r];
-            disp("e")
-            disp(e);
+
+            %la trasformata tra t e g
+            %prendo l'asse di rotazione h e theta con Rottoangleaxis
+
+            % r = O_b_g - O_b_t; % Compute position error
+            % 
+            % %% errore sull'angolo test
+            % bRt = T_b_t(1:3,1:3);
+            % bRg = bTg(1:3,1:3);
+            % Rerr = bRg * bRt'; % cambia l'ordine
+            % [h, theta] = RotToAngleAxis(Rerr);
+            % e = [h*theta; r];
+            % disp("e")
+            % disp(e);
             
             Ka = self.k_a * eye(3);
             Kl = self.k_l * eye(3);
             K = [Ka, zeros(3,3);
                  zeros(3,3), Kl];
             
-            x_dot = K * e; % Compute the control output
+            x_dot = K * b_e_tg; % Compute the control output
 
             
         end
